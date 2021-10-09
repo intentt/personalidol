b2cb7c00c8d381492b8326c2e5630d89550846ee
Mateusz Charytoniuk <mateusz.charytoniuk@protonmail.com>
2021-10-09 18:12:04 +0200

    chore: check for image complete when preloading

--- packages/dom/src/preloadImage.ts
+++ packages/dom/src/preloadImage.ts
@@ -3,13 +3,24 @@ import type { Progress } from "@personalidol/framework/src/Progress.interface";
 export function preloadImage(progress: Progress, textureUrl: string): Promise<HTMLImageElement> {
   return new Promise(function (resolve, reject) {
     const image = new Image();
+    let resolved: boolean = false;
 
     image.crossOrigin = "Anonymous";
     image.onerror = reject;
     image.onload = function () {
+      if (!resolved) {
+        return;
+      }
+
+      resolved = true;
       resolve(image);
     };
 
     image.src = textureUrl;
+
+    if (image.complete && !resolved) {
+      resolved = true;
+      resolve(image);
+    }
   });
 }
